743735955f1ef0a3a5918aa84507a55c53323a98
Sangeeta <sangeetaswainlucky@gmail.com>
2025-12-01 18:03:13 +0530

    app issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import store from "./redux/store";
+import { store } from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
