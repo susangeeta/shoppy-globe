1e21f5e09c8191d7ffaca5a4337e25b0b39e34fb
Sangeeta <sangeetaswainlucky@gmail.com>
2025-12-07 21:37:28 +0530

    updated

--- shoppy/src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import logo from "./logo.png";
-import logo1 from "./logo1.png";
-
-export { logo, logo1 };
--- shoppy/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="w-full h-[60vh] flex justify-center items-center">
-      <div className="loader"></div>
-    </div>
-  );
-};
-
-export default Loader;
